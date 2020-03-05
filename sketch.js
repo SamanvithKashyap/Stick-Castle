edb322e551197c11d0f7516dcdc133767ac2edc3
SamanvithKashyap <61279872+SamanvithKashyap@users.noreply.github.com>
2020-03-05 20:07:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(300,300);
+  
+}
+
+function draw() {
+  background(0);  
+  triangle(30,60,60,30,90,60);  
+  rect(40,60,40,200);
+  rect(80,100,100,160);
+  rect(180,60,40,200);
+  triangle(170,60,200,30,230,60);
+  rect(105,31,50,70);
+  triangle(95,31,130,0,165,31);
+  rect(120,210,20,50);
+  
+  
+}
